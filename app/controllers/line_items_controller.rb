aa3d8f53cbb3c226b49787914bdacf1368676276
Jose Fuentes Delgado <Jose@MacBook-Air-de-Jose.local>
2014-08-13 22:07:10 +0200

    F2

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -37,6 +37,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         # format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
         format.html { redirect_to store_url }
+        format.js {@current_item = @line_item}
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
